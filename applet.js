de43097079a0e4b8a9f4621935726f5bf7887072
Peter Till <tillpetya20@gmail.com>
2025-07-13 10:45:30 +0200

    Update applet.js

--- applet.js
+++ applet.js
@@ -1,5 +1,8 @@
-export async function run({ city }) {
+async function run({ city }) {
   const res = await fetch(`https://api.weatherapi.com/v1/current.json?key={{API_KEY}}&q=${city}`);
   const json = await res.json();
   return `🌤️ ${json.location.name}: ${json.current.temp_c}°C`;
 }
+
+
+module.exports = { run };
